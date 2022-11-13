b8f34cd1178855f61510e13dced2fc2bd40427bb
Anson Feng <59370291+AnsonFeng1999@users.noreply.github.com>
2022-11-13 14:14:52 -0500

    Fixed minor comliation error again.

--- A3/starter/utils.c
+++ A3/starter/utils.c
@@ -177,7 +177,7 @@ struct refraction_ind_stk *stackInsert(struct refraction_ind_stk *new_instance, 
 // input: the pointer of the current stack top
 // output: a double of the index from stack instance popped
 double stackPop(struct refraction_ind_stk *current_stack) {
-  if (!stack) return -1;
+  if (!current_stack) return -1;
   struct refraction_ind_stk *current_stack_top = current_stack;
   double leaving_index = current_stack_top->current_index;
   current_stack = current_stack->next;
